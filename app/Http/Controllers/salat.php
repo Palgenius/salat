a8d48b53e63e05250a71ce318acd2a968ac72fed
ali alsaidi <ali_aos1992@hotmail.com>
2019-08-28 18:55:23 +0300

    Update salat.php

--- app/Http/Controllers/salat.php
+++ app/Http/Controllers/salat.php
@@ -16,9 +16,9 @@ class salat extends Controller
         $times=$p->getTimesForToday(31.418889,34.351667,'GMT+3',null,PrayerTimes::LATITUDE_ADJUSTMENT_METHOD_ANGLE,PrayerTimes::MIDNIGHT_MODE_STANDARD,PrayerTimes::TIME_FORMAT_12hNS);
        // $today=Carbon::now()->toDateString();
        $ar_times=[
+           ['key'=>'الفجر','value'=>$times['Fajr']],
            ['key'=>'الشروق','value'=>$times['Sunrise']],
            ['key'=>'الظهر','value'=>$times['Dhuhr']],
-           ['key'=>'الفجر','value'=>$times['Fajr']],
            ['key'=>'العصر','value'=>$times['Asr']],
            ['key'=>'المغرب','value'=>$times['Maghrib']],
            ['key'=>'العشاء','value'=>$times['Isha']]
@@ -26,6 +26,62 @@ class salat extends Controller
                Date::setTranslation(new Arabic());
         $todayHijri=Date::today()->format('l d F o', Date::INDIAN_NUMBERS);;
         $carbon =Carbon::now()->toArray();
-        return response()->json(['ar_times'=>$ar_times,'times'=>$times,'hijri'=>$todayHijri,'carbon'=>$carbon]);
+        $array=['ar_times'=>$ar_times,'times'=>$times,'hijri'=>$todayHijri,'carbon'=>$carbon];
+
+        $timenow = Carbon::now()->toArray()['hour'] * 60 +  Carbon::now()->toArray()['minute'];
+
+            $fajr=intval(explode(':',$times['Fajr'])[0])*60 +  intval(explode(':',$times['Fajr'])[1]);
+            $dhuhr=intval(explode(':',$times['Dhuhr'])[0])*60 +  intval(explode(':',$times['Dhuhr'])[1]);
+            $asr=(intval(explode(':',$times['Asr'])[0])+12)*60 +  intval(explode(':',$times['Asr'])[1]);
+            $maghrib=(intval(explode(':',$times['Maghrib'])[0])+12)*60 +  intval(explode(':',$times['Maghrib'])[1]);
+            $isha=(intval(explode(':',$times['Isha'])[0])+12)*60 +  intval(explode(':',$times['Isha'])[1]);
+        if(
+            $fajr == $timenow ||  $dhuhr == $timenow ||  $asr == $timenow ||  $maghrib == $timenow ||  $isha == $timenow
+            ){
+            $array['adanAfter']=60 - Carbon::now()->toArray()['second'];
+        }
+
+        if(
+            $maghrib > $timenow  &&  $maghrib+10 < $timenow
+        ){
+            $before = $timenow+10 - $maghrib  ;
+            if($before==1)$before =60 - Carbon::now()->toArray()['second'];
+            $array['eqamaAfter']= $before;
+        }
+        if(
+            $isha > $timenow  && $isha+10 < $timenow
+        ){
+            $before = $timenow+10 - $fajr  ;
+            if($before==1)$before =60 - Carbon::now()->toArray()['second'];
+            $array['eqamaAfter']= $before;
+        }
+
+        if(
+          $fajr > $timenow  &&  $fajr+20 < $timenow
+        ){
+            $before = $timenow+20 - $fajr  ;
+            if($before==1)$before =60 - Carbon::now()->toArray()['second'];
+            $array['eqamaAfter']= $before;
+        }
+
+        if(
+
+            $dhuhr > $timenow   &&  $dhuhr+20 < $timenow
+        ){
+            $before = $dhuhr+20 - $timenow  ;
+            if($before==1)$before =60 - Carbon::now()->toArray()['second'];
+            $array['eqamaAfter']= $before;
+        }
+
+        if(
+            $asr > $timenow   && $asr + 20 < $timenow
+        ){
+
+            $before =   $asr+20  - $timenow;
+            if($before==1)$before =60 - Carbon::now()->toArray()['second'];
+            $array['eqamaAfter']= $before;
+        }
+
+        return response()->json($array);
     }
 }
