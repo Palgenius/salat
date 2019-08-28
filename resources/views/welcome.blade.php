23e12e8a4291b36b9cba5499c42a97c368ceda61
H-Shaban <H-Shaban@users.noreply.github.com>
2019-08-28 19:13:29 +0300

    dd

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -10,8 +10,10 @@
 	<!-- Latest compiled and minified CSS -->
 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 	<!-- Optional theme -->
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
-	<style type="text/css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap-theme.min.css">
+<link rel="stylesheet" href="{{ asset('font/style.css') }}">
+
+    <style type="text/css">
 		.list-times{
 			list-style: none;
     		text-align: center;
@@ -30,6 +32,17 @@
 		.prayer-times{
 			line-height: 4;
 		}
+        body{
+            background: url('http://localhost/salat/public/img/bg.png');
+            background-size: cover;
+            font-family:'Tajawal Regular' !important;
+            font-weight: Bold !important;
+            font-size:18px !important;
+        }
+        #ctime, #hijri, #melady{
+            color: white;
+            font-weight: Bold !important;
+        }
 	</style>
 
 
@@ -37,7 +50,7 @@
 <body>
 	<div class="container">
 		<div class='row'>
-			<div class="col-lg-4">
+			<div class="col-lg-4" style="background-color: azure; opacity: 0.8; box-shadow: 0px 2px 10px #888888;">
 				<h2 class="text-center prayer-times"> مواقيت الصلاة </h2>
 				<ul class="list-times">
 					<li>
@@ -67,11 +80,11 @@
 				</ul>
 			</div>
 			<div class="col-lg-8">
-				<h2 class="text-center" id="ctime"> </h2>
-				<h2 class="text-center"> <span id="hijri"></span> | <span id="melady"></span></h2>
+				<h1 class="text-center" id="ctime"> </h1>
+				<h1 class="text-center"> <span id="hijri"></span> | <span id="melady"></span></h1>
 				<div class="img-thumble">
 					<img class="img img-responsive img-round img-thumble"
-					src="abandoned-forest-hd-wallpaper-34950.jpg">
+                src="">
 				</div>
 			</div>
 		</div>
@@ -83,7 +96,7 @@
                 axios.get('http://localhost/salat/public/salat')
                 .then(function (response) {
                     // handle success
-                    console.log(response.data);
+                    //console.log(response.data);
                     if(response.data.carbon.hour>12)
                     $("#ctime").text((response.data.carbon.hour-12)+':'+response.data.carbon.minute+':'+response.data.carbon.second);
                     else
@@ -125,7 +138,7 @@ function formatDate(date) {
 
 function azan(){
     var sound = new Howl({
-      src: ['https://ia800709.us.archive.org/14/items/Du3a_uP_bY_mUSLEm/046--_up_by_muslem.mp3'],
+      src: ['{{ asset("mp3/a.mp3") }}'],
       volume: 0.5,
       onend: function () {
         alert('Finished!');
